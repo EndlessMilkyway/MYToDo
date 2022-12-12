f3885a2ecb96fbdb8899c145298100daed44783d
KYR <freekyr7529@gmail.com>
2022-12-12 11:37:35 +0900

    feature : 중요 프래그먼트 java 코드 기초 작성

---
+++ app/src/main/java/com/endlessmilkyway/mytodo/ImportantFragment.java
@@ -0,0 +1,42 @@
+package com.endlessmilkyway.mytodo;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImportantFragment extends Fragment {
+    private Context mContext;
+    private ToDoItemDB database;
+    private List<ToDoItem> toDoImportantData = new ArrayList<>();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_important, container, false);
+        mContext = getActivity().getApplicationContext();
+
+        RecyclerView toDoImportantList = rootView.findViewById(R.id.toDoImportantList);
+
+        this.initializedData();
+
+        toDoImportantList.setLayoutManager(new LinearLayoutManager(mContext));
+        toDoImportantList.setAdapter(new ToDoItemAdapter(getActivity(), toDoImportantData));
+
+        return rootView;
+    }
+
+    private void initializedData() {
+        database = ToDoItemDB.getInstance(mContext);
+        toDoImportantData = database.toDoItemDao().getImportant();
+    }
+}

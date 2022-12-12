0ec0dd7ac9976df85753ebe81cf3b4ecf71b3c05
KYR <freekyr7529@gmail.com>
2022-12-12 10:12:38 +0900

    feature : 스플래쉬 액티비티 구현

--- app/src/main/java/com/endlessmilkyway/mytodo/SplashActivity.java
+++ app/src/main/java/com/endlessmilkyway/mytodo/SplashActivity.java
@@ -2,13 +2,15 @@ package com.endlessmilkyway.mytodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
-@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
+    SharedPreferences pref;
+
     int SHOW_DURATION = 1000;
 
     @Override
@@ -16,12 +18,23 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        transferToMain(SHOW_DURATION);
+        Intent intent = inspectFirstExecution();
+
+        transferToMain(SHOW_DURATION, intent);
     }
 
-    private void transferToMain(int sec) {
+    private Intent inspectFirstExecution() {
+        pref = getSharedPreferences("isFirst", Activity.MODE_PRIVATE);
+        boolean first = pref.getBoolean("isFirst", false);
+
+        if (!first) {
+            return new Intent(getApplicationContext(), InitActivity.class);
+        }
+        return new Intent(getApplicationContext(), MainActivity.class);
+    }
+
+    private void transferToMain(int sec, Intent intent) {
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
         }, sec);
